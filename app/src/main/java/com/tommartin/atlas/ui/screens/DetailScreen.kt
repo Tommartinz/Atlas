907a43252595ab7e277b307420cdf6d71b01e3f7
Tomartin <tomasguimartinez@gmail.com>
2024-05-09 09:35:32 -0300

    Initial DetailScreen.kt Design

--- app/src/main/java/com/tommartin/atlas/ui/screens/DetailScreen.kt
+++ app/src/main/java/com/tommartin/atlas/ui/screens/DetailScreen.kt
@@ -2,22 +2,36 @@ package com.tommartin.atlas.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
-import com.tommartin.atlas.data.model.BasicCountry
-import com.tommartin.atlas.data.model.DetailedCountry
+import coil.compose.AsyncImage
 import com.tommartin.atlas.ui.AtlasViewModel
-import kotlinx.coroutines.launch
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailScreen(officialName: String, viewModel: AtlasViewModel, navHostController: NavHostController) {
 
@@ -26,19 +40,55 @@ fun DetailScreen(officialName: String, viewModel: AtlasViewModel, navHostControl
     LaunchedEffect(navHostController.currentBackStackEntry) {
         viewModel.getDetailedCountry(officialName)
     }
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colorScheme.background
-    ) {
-        Column(
+
+    if (uiState.value.detailedCountry == null) {
+        Surface(
             modifier = Modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
+            color = MaterialTheme.colorScheme.background
         ) {
-            if (uiState.value.detailedCountry == null) {
-                Text(text = "Nothing here")
-            } else {
-                Text(text = uiState.value.detailedCountry!![0].region)
+            Text(text = "no info")
+        }
+    } else {
+        Scaffold(
+            topBar = {
+                LargeTopAppBar(
+                    title = {
+                        Row(
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            verticalAlignment = Alignment.CenterVertically,
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Text(text = uiState.value.detailedCountry!![0].name.common)
+                            AsyncImage(
+                                model = uiState.value.detailedCountry!![0].flags.svg,
+                                contentDescription = null,
+                                modifier = Modifier.size(36.dp).clip(CircleShape),
+                                contentScale = ContentScale.Crop
+                            )
+                        }
+                    },
+                    navigationIcon = {
+                        IconButton(onClick = { /*TODO*/ }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
+                                contentDescription = null
+                            )
+                        }
+                    }
+                )
+            }
+        ) { paddingValues: PaddingValues ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(
+                        top = paddingValues.calculateTopPadding(),
+                        start = 16.dp,
+                        end = 16.dp
+                    ),
+                verticalArrangement = Arrangement.SpaceEvenly,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
             }
         }
     }
